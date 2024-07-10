3cf5f75ed6f95f6f2ea01a0fc3f7564d2f96a009
Kelly Ramos <kramos@fcc.org.br>
2024-07-10 14:28:33 -0300

    style: typography config

--- src/App.tsx
+++ src/App.tsx
@@ -8,26 +8,12 @@ function App() {
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+     <p>rotas</p>
+     <h1>Titulo 1</h1>
+     <h2>Titulo 2</h2>
+     <h3>Titulo 3</h3>
+     <h4>Titulo 4</h4>
+     <h5>Titulo 5</h5>
     </>
   )
 }
