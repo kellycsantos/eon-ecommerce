bbba56371c5f992f55fc7a679e39cd26c14d54f3
Kelly Ramos <kramos@fcc.org.br>
2024-07-11 17:06:28 -0300

    feat(navbar): structured navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,25 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
 import './App.scss'
 import Navbar from './components/Navbar'
+let y = window.screen.height
+window.addEventListener('scroll', (e) =>{
+  console.log('Tamanho telaaa', y )
+  console.log('scroll', e)
+  console.log('scroll prima', e.target?.scrollingElement.scrollTop)
+  console.log('Tamanho do scroll', e.target?.scrollingElement.scrollHeight)
 
+  
+  // console.log('scroll duo', e.scrollingElement.scrollHeight )
+})
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
+    <Navbar/>
+    <div style={{height: '100vh', backgroundImage: 'linear-gradient(#e66465, #9198e5)', position: 'relative'}}>
+    </div>
      <p>rotas</p>
      <h1>Titulo 1</h1>
      <h2>Titulo 2</h2>
@@ -17,7 +28,6 @@ function App() {
      <h5>Titulo 5</h5>
      <h1>Titulo 1</h1>
      <h2>Titulo 2</h2>
-    <Navbar/>
      <h3>Titulo 3</h3>
      <h4>Titulo 4</h4>
      <h5>Titulo 5</h5>
--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -1,19 +1,40 @@
+import { useState } from 'react'
 import eonWhiteIcon from '../../assets/eon-white.svg'
 import './navbar.scss'
 export default function Header() {
+/*
+    classe transparente só vai ser ativada quando:
+    - estiver na home
+    - valor do scroll for menor que o banner
+*/
+
+const [visibleMenu, setVisibleMenu ] = useState(false)
+
+function showMenu(){
+    setVisibleMenu(!visibleMenu)
+}
+
 
     return (
         <>
-            <nav className='principalNav'>
-                <a href=''>
-                <img src={eonWhiteIcon} />
-
+            <nav className={'principalNav'}>
+                <a href='#'>
+                <img className='menu-icon' src={eonWhiteIcon} />
                 </a>
-                <ul>
+                <menu style={{display: `${visibleMenu ? 'flex' : 'none'}`}}>
                     <li><a href="#">Inicio</a></li>
                     <li><a href="#">Produtos</a></li>
                     <li><a href="#">Suporte</a></li>
-                </ul>
+                </menu>
+
+                <section>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                </section>
+
+
+                <button onClick={showMenu}>X</button>
             </nav>
 
         </>
