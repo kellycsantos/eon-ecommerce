797c766ae3217bd94dcc31d0510ff489c149c8bc
Kelly Ramos <kramos@fcc.org.br>
2024-07-12 17:27:10 -0300

    chore:  estrutura iniciada

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -2,26 +2,35 @@ import { useState } from 'react'
 import eonWhiteIcon from '../../assets/eon-white.svg'
 import './navbar.scss'
 export default function Header() {
-/*
-    classe transparente só vai ser ativada quando:
-    - estiver na home
-    - valor do scroll for menor que o banner
-*/
+    /*
+        classe transparente só vai ser ativada quando:
+        - estiver na home
+        - valor do scroll for menor que o banner
+    */
 
-const [visibleMenu, setVisibleMenu ] = useState(false)
+    const [hideMenu, sethideMenu] = useState(false)
 
-function showMenu(){
-    setVisibleMenu(!visibleMenu)
-}
+    function showMenu() {
+        console.log('Valor', hideMenu)
+        sethideMenu(!hideMenu)
+        // criar uma classe e adicionar ela no 
+        // body para impedir o scroll
+        
+        // hideMenu === true ?
+        // document.body.style.overflowY = 'scroll' :
+        // document.body.style.overflowY = 'hidden'
+        console.log('Valor', hideMenu)
+    }
 
 
     return (
         <>
             <nav className={'principalNav'}>
+              {/* <p style={{color: 'yellow'}}>{visibleMenu ? 'Ola' : 'Tchau'}</p>   */}
                 <a href='#'>
-                <img className='menu-icon' src={eonWhiteIcon} />
+                    <img className='menu-icon' src={eonWhiteIcon} />
                 </a>
-                <menu style={{display: `${visibleMenu ? 'flex' : 'none'}`}}>
+                <menu className={`${hideMenu ? 'showNav' : 'hideNav'}`}>
                     <li><a href="#">Inicio</a></li>
                     <li><a href="#">Produtos</a></li>
                     <li><a href="#">Suporte</a></li>
