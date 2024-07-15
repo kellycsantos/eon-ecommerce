65daeb25f23c68b27a904f876404e4064c16b910
Kelly Ramos <kellycristinasntos@gmail.com>
2024-07-15 17:08:27 -0300

    feat (categories) : categories component started

--- src/views/Home.tsx
+++ src/views/Home.tsx
@@ -1,9 +1,11 @@
 import Banner from "../components/Banner"
 import OfferImage from '../assets/images/banner/principal_banner.png'
+import Categories from "../components/Categories/Categories"
 export default function HomeView(){
     return(
         <>
           <Banner style='gradient' image={OfferImage}/>
+          <Categories/>
         </>
     )
 }
