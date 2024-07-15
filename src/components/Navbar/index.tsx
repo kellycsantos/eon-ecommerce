83fa65882ef0590ddd2db43bbbc8d9d3c70a50fe
Kelly Ramos <kellycristinasntos@gmail.com>
2024-07-15 16:17:13 -0300

    feat (navbar) : Navlink config

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -1,4 +1,6 @@
 import { useState } from 'react'
+import { NavLink } from 'react-router-dom'
+
 import eonWhiteIcon from '../../assets/eon-white.svg'
 import searchIcon from '../../assets/icons/icon _search.svg'
 import cartIcon from '../../assets/icons/icon _cart.svg'
@@ -9,10 +11,14 @@ import './navbar.scss'
 export default function Header() {
 
     const [hideMenu, sethideMenu] = useState(false)
+    const links = [
+        { title: 'Inicio', url: '/' },
+        { title: 'Produtos', url: '/produtos' },
+        { title: 'Suporte', url: '/suporte' },
+    ]
 
     function showMenu() {
         const body = document.querySelector('body')
-        const menu = document.querySelector('#links_container')
         sethideMenu(!hideMenu)
         if (hideMenu === true) {
             body?.classList.remove('scrollBlock')
@@ -21,28 +27,24 @@ export default function Header() {
         }
     }
 
-
     return (
         <>
             <nav className={'principalNav'}>
-                {/* <p style={{color: 'yellow'}}>{visibleMenu ? 'Ola' : 'Tchau'}</p>   */}
-                {/* <button className='menu-icon' onClick={showMenu}>E</button> */}
                 <a href='#' className='menu-icon'>
                     <img src={eonWhiteIcon} />
                 </a>
                 <menu id='links_container' className={`${hideMenu ? 'showNav' : 'hideNav'}`}>
-                    <li><a href="#">Inicio</a></li>
-                    <li><a href="#">Produtos</a></li>
-                    <li><a href="#">Suporte</a></li>
+                    {
+                        links.map((link, index) =>
+                            <NavLink to={link.url} key={index}>{link.title}</NavLink>
+                        )
+                    }
                 </menu>
                 <section className='menu_buttons'>
                     <button style={{ backgroundImage: `url(${searchIcon})` }}></button>
                     <button style={{ backgroundImage: `url(${cartIcon})` }} ></button>
                     <button className={`menu_icon ${hideMenu ? 'menuCloseIcon' : 'menuOpenIcon'}`}
                         onClick={showMenu}></button>
-                    {/* <button className='menu_icon' 
-                        style={{ backgroundImage: `url(${hideMenu ? menuCloseIcon : menuIcon})` }} 
-                        onClick={showMenu}></button> */}
                     <button className='user_icon' style={{ backgroundImage: `url(${userIcon})` }} onClick={showMenu}></button>
                 </section>
             </nav>
