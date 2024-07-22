122b4ff26e5d2c64a72b936192ca80b11e8e0787
Kelly Ramos <kellycristinasntos@gmail.com>
2024-07-22 10:33:15 -0300

    feat: product map

--- src/views/Home.tsx
+++ src/views/Home.tsx
@@ -1,11 +1,30 @@
 import Banner from "../components/Banner"
 import OfferImage from '../assets/images/banner/principal_banner.png'
 import Categories from "../components/Categories"
-export default function HomeView(){
-    return(
-        <>
-          <Banner style='gradient' image={OfferImage}/>
-          <Categories/>
-        </>
-    )
+import echodot from '../assets/images/products/echodot.png'
+import iphone from '../assets/images/products/iphone.png'
+import macbook from '../assets/images/products/macbook.png'
+import playstation from '../assets/images/products/playstation.png'
+import smart_watch from '../assets/images/products/smart_watch.png'
+import Card from "../components/ProductCard"
+
+const products = [
+  { img: echodot, name: 'Echo Dot 2.0', price: '327,40' },
+  { img: iphone, name: 'Iphone 14 Plus', price: '4.889' },
+  { img: macbook, name: "Macbook Pro '13 Cinza espacial", price: '6.799' },
+  { img: playstation, name: 'Playstation 5 Slim 1 TB', price: '3499' },
+  { img: smart_watch, name: 'SmartWatch', price: '356' },
+]
+export default function HomeView() {
+  return (
+    <>
+      <Banner style='gradient' image={OfferImage} />
+      <Categories />
+
+      {
+        products.map((product, index) => <Card key={index} name={product.name} price={product.price} img={product.img} />)
+      }
+
+    </>
+  )
 }
