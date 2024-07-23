23ba3f51fd4e7b04b04647d76cc64479c47511ca
Kelly Ramos <kellycristinasntos@gmail.com>
2024-07-23 19:47:39 -0300

    style: add wrapper image and ellipsis to product name

--- src/components/ProductCard/index.tsx
+++ src/components/ProductCard/index.tsx
@@ -1,11 +1,13 @@
 import './product-card.scss'
 import ICard from '../../interfaces/Icard'
-export default function Card({name,price, img}: ICard){
-    return(
+export default function Card({ name, price, img }: ICard) {
+    return (
         <section className='product-card'>
-            <img src={img} alt="Macbook pro" />
+            <div className='img-wrapper'>
+                <img src={img} alt="Macbook pro" />
+            </div>
             <h5>{name}</h5>
-            <h6>R$ {price}.00</h6>
+            <h6>R$ {price}</h6>
         </section>
     )
 }
