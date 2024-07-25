67c4f6e548f3f0db1f0f77285638cfa0f8dc5940
Kelly Ramos <kellycristinasntos@gmail.com>
2024-07-25 19:02:02 -0300

    created grid gallery

--- src/components/GridImage/index.tsx
+++ src/components/GridImage/index.tsx
@@ -1,7 +1,20 @@
+import './gallery.scss'
+import iphone from '../../assets/images/gallery/iphone.png'
+import headset from '../../assets/images/gallery/headset.png'
+import notebook from '../../assets/images/gallery/notebook.png'
+import aspirador from '../../assets/images/gallery/aspirador.png'
+import amazon from '../../assets/images/gallery/smart_amazon.png'
 export default function GridImages(){
     return(
-        <>
-            <p>Grid images component !</p>
-        </>
+        <div className='gallery'>
+            
+
+                <img src={iphone} className='first'/>
+                <img src={headset} className='second'/>
+                <img src={notebook} className='third'/>
+                <img src={aspirador} className='fourth'/>
+                <img src={amazon} className='fifth'/>
+           
+        </div>
     )
 }
