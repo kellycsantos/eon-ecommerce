e7225c7648f2123d82cc85d8eeead69075c50e2e
Kelly Ramos <kellycristinasntos@gmail.com>
2024-08-05 16:59:06 -0300

    feat (Button) : dynamic text and functions

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,6 +1,7 @@
 import './button.scss'
-export default function Button(){
+import { IButton } from '../../interfaces/Ibutton'
+export default function Button({text, click} : IButton){
     return(
-        <button className='btn-component'>Clique !</button>
+        <button className='btn-component' onClick={click}>{text ? text : 'Clique me'}</button>
     )
 }
