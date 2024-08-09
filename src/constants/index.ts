49219bccc5e1c81b461a00d55375cb3865a5a69c
Kelly Ramos <kellycristinasntos@gmail.com>
2024-08-09 14:44:41 -0300

    docs : add consts

---
+++ src/constants/index.ts
@@ -0,0 +1,42 @@
+export const Categories = [
+    {
+        id: 0,
+        categoryName: 'Todos os produtos'
+    },
+    {
+        id: 1,
+        categoryName: 'Eletrodomesticos'
+    },
+    {
+        id: 2,
+        categoryName: 'Fitness'
+    },
+    {
+        id: 3,
+        categoryName: 'Gadges'
+    },
+    {
+        id: 4,
+        categoryName: 'Audio e video'
+    },
+    {
+        id: 5,
+        categoryName: 'Notebooks'
+    },
+    {
+        id: 6,
+        categoryName: 'Casa inteligente'
+    },
+    {
+        id: 7,
+        categoryName: 'Tv'
+    },
+    {
+        id: 8,
+        categoryName: 'Smartphone'
+    },
+    {
+        id: 9,
+        categoryName: 'Video games'
+    },
+]
